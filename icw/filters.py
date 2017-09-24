5b42a48a9446c89cf491bf80edc00d256074f374
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2017-09-24 16:02:41 +0100

    Allow filtering on numbers of vehicles/casualties

--- icw/filters.py
+++ icw/filters.py
@@ -8,7 +8,10 @@ class AccidentFilter(django_filters.FilterSet):
     casualty_distribution = django_filters.ModelMultipleChoiceFilter(queryset=models.CasualtyDistribution.objects.order_by('-count'))
     vehicle_distribution = django_filters.ModelMultipleChoiceFilter(queryset=models.VehicleDistribution.objects.order_by('-count'))
     citations = django_filters.BooleanFilter()
+    number_of_vehicles = django_filters.NumericRangeFilter()
+    number_of_casualties = django_filters.NumericRangeFilter()
 
     class Meta:
         model = models.Accident
-        fields = ['date', 'severity', 'citations', 'casualty_distribution', 'vehicle_distribution']
+        fields = ['date', 'severity', 'citations', 'number_of_vehicles', 'number_of_casualties',
+                  'casualty_distribution', 'vehicle_distribution']
