277d5892549059e836a448e0878b364a844d90d1
Alexander Dutton <alexander.dutton@it.ox.ac.uk>
2017-10-08 15:27:52 +0100

    Added bbox filter to Accident

--- icw/filters.py
+++ icw/filters.py
@@ -1,6 +1,35 @@
 import django_filters
+from django.contrib.gis.geos import Polygon
+from django import forms
+from django.core.exceptions import ValidationError
 
 from . import models
+
+
+class BoundingBoxField(forms.Field):
+    def to_python(self, value):
+        if value:
+            try:
+                values = [float(v.strip()) for v in value.split(',')]
+            except ValueError:
+                raise ValidationError("Value should be four floats, delimited by commas")
+            if len(values) != 4:
+                raise ValidationError("Value should be four floats, delimited by commas")
+            return Polygon(((values[0], values[1]),
+                            (values[2], values[1]),
+                            (values[2], values[3]),
+                            (values[0], values[3]),
+                            (values[0], values[1])))
+
+
+class PointFilter(django_filters.Filter):
+    field_class = BoundingBoxField
+
+    def filter(self, qs, value):
+        if value:
+            lookup = "{}__coveredby".format(self.name)
+            qs = self.get_method(qs)(**{lookup: value})
+        return qs
 
 
 class AccidentFilter(django_filters.FilterSet):
@@ -10,13 +39,15 @@ class AccidentFilter(django_filters.FilterSet):
     has_citations = django_filters.BooleanFilter()
     number_of_vehicles = django_filters.RangeFilter()
     number_of_casualties = django_filters.RangeFilter()
+    bbox = PointFilter(name='location',
+                       help_text='The coordinate values of two opposite corners of a bounding box, as "<SW lng>,<SW lat>,<NE lng>,<NE lat>"')
 
     class Meta:
         model = models.Accident
         fields = ['date', 'severity', 'has_citations', 'number_of_vehicles', 'number_of_casualties',
                   'police_force', 'highway_authority',
                   'carriageway_hazards', 'vehicles__hit_object_in_carriageway', 'vehicles__hit_object_off_carriageway',
-                  'casualty_distribution', 'vehicle_distribution']
+                  'casualty_distribution', 'vehicle_distribution', 'bbox']
 
 
 class CasualtyFilter(django_filters.FilterSet):
